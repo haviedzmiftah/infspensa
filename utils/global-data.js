5fb7e25cd53aff7054e61c2c4c07795a19e2e751
haviedzmiftah <haviedzmiftah@gmail.com>
2024-10-25 22:55:55 +0700

    commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Informatika Spensa';
+    : 'Cyber Sp1ng';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'INFORMATIKA SMPN 1 NGAWI';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Team cyber Sp1ng.';
 
   return {
     name,
