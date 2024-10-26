8394c63d16dcd57874d2051e9a60e468494352e5
haviedzmiftah <haviedzmiftah@gmail.com>
2024-10-26 09:24:53 +0700

    261024

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      {/* <div className="block w-12 h-12 mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
+        <img className="object-center w-4 h-5" src="../img/smpn1ngawi.png"/>
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
